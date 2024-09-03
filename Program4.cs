6f407eab910db2f8da47226160570c89f0bec09f
Anastasiya Lakhmakova <anastasiya.lakhmakova@gmail.com>
2024-09-03 16:38:58 +0200

    Övning4: Vektorer och Sökning
    Detta är en övningsuppgift där vi ska göra en ny Ryggsäck, ganska lik den vi gjorde i övning 2. Men istället för att kunna spara ett enstaka strängvärde ska vi nu arbeta med en strängvektor istället, där vi ska spara upp till fem saker. Vi ska arbeta med en inmatning, utskrift och sökning av fem element i en strängvektor. För att sammanfatta;
    
    Vi ska bygga ett program ganska likt det vi gjorde i övning 2, men istället för en enstaka strängvariabel arbetar vi nu istället med en strängvektor som har fem element. Användaren ska kunna spara fem saker, skriva ut de fem sparade sakerna, och söka bland dem. För att sammanfatta;
    
    Vi ska arbeta med en strängvektor som har fem element
    I menyval 1 ska användaren kunna tilldela ett strängvärde till varje element. Använd en for-loop
    I menyval 2 ska användaren kunna skriva ut varje element. Använd en for-loop.
    I menyval 3 ska användaren kunna skriva ett sökord, som jämförs med varje element. Om sökningen lyckas ska de få ett meddelande. Använd en for-loop.
    I menyval 4 ska programmet avslutas.
    Vidare detaljer;
    
    En vektor (array) där användaren kan mata in upp till fem stycken föremål. Observera att inmatningarna inte måste göras en och en som i övning 2, det går fint att använda en for-loop som tilldelar vardera objekt en ReadLine på en gång. Kapitel 12.1.
    Utskriften av innehållet, detta gör du med hjälp av for-loopen. Kapitel 12.3.
    Menyn ska vara skriven med en SWITCH-sats. Kapitel 4.3.
    Ditt program ska hantera felaktig inmatning så att du undviker att programmet kraschar. Kapitel 10.3, MSDN - TryParse
    Linjär sökning av innehållet i ryggsäcken. Kapitel 14.1 till 14.2
    Du kommenterar din kod och förklarar dina val och hur det fungerar.
    När du är klar med detta kan du arbeta med följande om du vill utöka programmets funktionalitet
    
    Kunna rensa innehållet i ryggsäcken.
    Att skriva lösningen med en klass/metod.
    Använd ToUpper/ToLower för att användaren inte ska behöva tänka på stora eller små bokstäver.

---
+++ Program4.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MyApp
+{
+    internal class Program4
+    {
+        //metoder
+        static void VisaMeny()
+        {
+            Console.WriteLine("\nVälkommen till ryggsäcken!");//detta är programmenyn, som består av 5 punkter
+            Console.WriteLine("\n[1] Lägg till 5 föremål i din ryggsäck");
+            Console.WriteLine("[2] Skriv ut innehållet");
+            Console.WriteLine("[3] Sök efter föremålet i ryggsäcken");
+            Console.WriteLine("[4] Rensa innehållet av ryggsäcken");
+            Console.WriteLine("[5] Avsluta");
+            Console.Write("\n\tVälj: ");
+        }
+        static string[] LäggTill()
+        {
+            string[] vektor = new string[5];
+            Console.Write("\n\tLägg till 5 föremål i din ryggsäck: ");
+
+            for (int i = 0; i < vektor.Length; i++) // Jag använder en for-loop för att tilldela ett strängvärde till varje element
+            {
+                Console.Write("\nFöremål " + (i + 1) + ": ");
+                vektor[i] = Console.ReadLine();
+            }
+
+            return vektor;
+        }
+
+        static string[] VisaInnehållet(string[] vektor) 
+        {
+            Console.WriteLine("\n\tRyggsäcken innehåller: ");
+
+            for (int i = 0; i < vektor.Length; i++)//Jag använder en for-loop för att skriva ut varje element
+            {
+                if (vektor[i] == null)//Om användaren inte har lagt något i ryggsäcken, men vill se vad den innehåller, informerar programmet användaren om att ryggsäcken är tom
+                {
+                    Console.WriteLine("\n\tRyggsäcken är tom nu");
+                    break;
+                }
+                else if (vektor[i] == "")//om det finns tom strängar i ryggsäcken, kommer programmet att informera 
+                    Console.WriteLine(i + 1 + ") Det finns ingenting här");
+
+                else if (vektor[i] != null && vektor[i] != "")//om det finns föremål i ryggsäcken (inte null eller mellanslag), kommer programmet att visa innehållet i ryggsäcken
+                    Console.WriteLine((i + 1) + ") " + vektor[i]);
+            }
+            return vektor;
+        }
+        static string[] Sök(string[] vektor)
+        {
+            bool minBool = false;
+
+            Console.WriteLine("\n\tVad vill du hitta i ryggsäcken? Skriv ett sökord: ");
+
+            string sökord = Console.ReadLine();//Jag sparar sökordet som användaren har skrivit i en variabel [sökord]
+
+            for (int i = 0; i < vektor.Length; i++)//Linjär sökning av innehållet i ryggsäcken
+            {
+
+                if (vektor[i] != null && vektor[i] != "" && sökord.ToUpper() == vektor[i].ToUpper())//Jag använder ToUpper för att användaren inte ska behöva tänka på stora eller små bokstäver.
+                {
+                    Console.WriteLine("\n\tSökningen lyckades! " + "Föremålet '" + sökord + "' som du letar efter finns på plats " + (i + 1));//Jag presenterar sökresultatet för användaren
+                    minBool = true;
+                    break;
+                }
+            }
+
+            if (minBool == false)//Programmet informerar användaren om inget hittas efter jämförelse av sökordet med varje element eller om användaren har tryckt på mellanslagstangenten istället för att skriva in sökordet.
+            {
+                Console.WriteLine("\n\tInget sådant föremål hittades i ryggsäcken eller ryggsäcken är tom nu");
+            }
+            return vektor;  
+        }
+        static string[] RensaInnehållet (string[] vektor)
+        {
+            for (int i = 0; i < vektor.Length; i++)//Jag använder en for-loop för att rensa innehållet av varje element
+            {
+                vektor[i] = string.Empty;
+            }
+            Console.WriteLine("\n\tRyggsäcken är tom nu");
+            return vektor;
+        }
+
+        static string SkrivUt(string text)//metoden som skriver ut en text
+        {
+            Console.WriteLine(text);
+            return text; 
+        
+        }
+
+        //Det är här programmet genomförs
+
+        static void Main(string[] args)
+        {
+
+            bool b = true;//Jag skapar en variabel "b" som ska styra while-loopen - om variabelns värde är true körs loopen, om variabelns värde är false avslutas loopen
+            string[] minVektor = new string[5];//Jag skapar en strängvektor som har fem element
+
+            while (b)//Jag skapar en while-loop för att användaren alltid ska ha tillgång till programmenyn, utom när användaren vill avsluta programmet.
+            {
+
+                VisaMeny();
+
+                bool lyckad = Int32.TryParse(Console.ReadLine(), out int tillvalen);//Här väljer användaren ett menval och jag lagrar detta värde i en variabel "tillvalen" och
+                                                                                    //mitt program hanterar felaktig inmatning så att jag undviker att programmet kraschar
+                if (lyckad)
+                {
+
+                    switch (tillvalen)//Jag skapar en switch-sats för att användaren kan använda programmenyn
+                    {
+                        case 1://det är menyval 1, där användaren kan tilldela ett strängvärde till varje element. 
+
+                            minVektor = LäggTill();
+                            break;
+
+                        case 2://det är menyval 2, där användaren kan skriva ut varje element.
+
+                            VisaInnehållet(minVektor);
+                            break;
+
+                        case 3://det är menyval 3, där användaren kan skriva ett sökord, som jämförs med varje element. Om sökningen lyckas ska de få ett meddelande. Använd en for-loop.
+
+                            Sök(minVektor);
+                            break;
+
+                        case 4://det är menyalternativ 4, där användaren kan rensa innehållet av ryggsäcken
+                            
+                            RensaInnehållet(minVektor);
+                            break;
+
+                        case 5://det är menyval 5, där ska programmet avslutas
+                            b = false;
+                            break;
+
+                        default://detta beskriver vad programmet ska göra om användaren väljer en punkt som inte finns i programmenyn
+                            SkrivUt("\n\tDu måste välja menyalternativ 1-5");
+                            break;
+                    }
+                }
+                else {
+                    SkrivUt("\n\tEndast siffrorna 1-5 kan användas för att välja ett menyval");
+                }
+            }
+        }
+    }
+}
